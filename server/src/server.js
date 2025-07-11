46dddbd29f464556bddbe6d9563001b9a7f152d2
Satyajit Samal <samalsatyajit668@gmail.com>
2025-07-11 15:06:48 +0530

    vercel config, db connection in server.js

--- server/src/server.js
+++ server/src/server.js
@@ -1,20 +1,22 @@
-import mongoose from "mongoose";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 import app from "./app.js";
 
 dotenv.config({ path: "./src/config/.env" });
 
+let isConnected = false;
+
 const connectDB = async () => {
+  if (isConnected) return;
   try {
     await mongoose.connect(process.env.DB_URI);
+    isConnected = true;
     console.log("DB connected successfully!");
   } catch (error) {
     console.error(`MongoDB connection failed ${error.message}`);
   }
 };
 
-const PORT = process.env.PORT || 3000;
-const server = app.listen(PORT, async () => {
-  await connectDB();
-  console.log(`Server running on port ${PORT}`);
-});
+connectDB();
+
+export default app;
